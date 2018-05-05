5f847b52cf78fa59e63ad9bb3f88c6bd7e429fc9
karlissonJR <karlissonj@gmail.com>
2018-05-05 15:54:14 -0300

    tela 1

--- components/style.js
+++ components/style.js
@@ -26,49 +26,6 @@ const styles = StyleSheet.create({
     height: 110,
   },
 
-  telaInicial: {
-    flex: 1,
-  },
-
-  telaInicialView1: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: '#008B8B',
-  },
-
-  telaInicialView2: {
-    flex: 5,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  telaInicialView3: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  telaInicialView4: {
-    flex: 8,
-  },
-
-  boaAcaoTxt3: {
-    color: 'white',
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-
-  imagemLampada2: {
-    width: 30,
-    height: 30,
-  },
-
-  imagemTresPontos: {
-    width: 30,
-    height: 30,
-  },
-
 });
 
 export default styles;
